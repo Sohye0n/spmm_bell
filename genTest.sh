fed896c992fd30649adb92f3a7f12c7e674e7fd2
sohye0n <sbb0416@naver.com>
2024-11-17 20:52:21 +0900

    타입별로 실행 가능하게 마스크 추가

--- genTest.sh
+++ genTest.sh
@@ -4,6 +4,7 @@ size1=$1
 size2=$2
 blockSize=$3
 rhsCol=$4
+runMask=$5
 
 echo $size1 $size2 $blockSize $rhsCol
 
@@ -55,6 +56,11 @@ done
 command_default1="-R $size1 -C $size2 -B $blockSize --mode exclusive"
 command_default2="-R $size2 -C $size1 -B $blockSize --mode exclusive"
 
+# runMask에서 각 값을 배열로 변환
+mask=($(echo "$runMask" | grep -o .))  # 각 숫자를 배열 요소로 저장
+echo $runMask
+echo "${mask[5]}"
+
 for type in ${types[@]}; do
     echo "---- $type ----"
     if [[ $type == ${types[0]} ]]; then
@@ -63,261 +69,262 @@ for type in ${types[@]}; do
         command_default=$command_default2
     fi
 
-    # ######## 실행 - skinny #######
-    # echo "---- skinny----"
-    # csv_file="${type}${subdirs[0]}.csv"
-    # echo "this $csv_file"
+    if [ "${mask[0]}" -eq "1" ]; then
 
-    # # 데이터셋 생성
-    # echo "generating dataset for skinny"
-    # python3 ./util/gen_skinny.py $command_default
+        ######## 실행 - skinny #######
+        echo "---- skinny----"
+        csv_file="${type}${subdirs[0]}.csv"
+        echo "this $csv_file"
 
-    # # 실행시간 측정
-    # for i in {1..3}; do
-    #     echo "set $i"
-    #     results=()
-    #     for file in $type/skinny/*.mtx; do
-    #         if [ -f "$file" ]; then
-    #             echo "$file"
-    #             base_name=$(basename "$file")
-    #             A=$(echo "$base_name" | cut -d'_' -f1 | sed 's/^m//') 
-    #             B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
+        # 데이터셋 생성
+        echo "generating dataset for skinny"
+        python3 ./util/gen_skinny.py $command_default
 
-    #             # 파일에서 ./data 부분 제거
-    #             relative_file="${file#./data}"
+        # 실행시간 측정
+        for i in {1..3}; do
+            echo "set $i"
+            results=()
+            for file in $type/skinny/*.mtx; do
+                if [ -f "$file" ]; then
+                    echo "$file"
+                    base_name=$(basename "$file")
+                    A=$(echo "$base_name" | cut -d'_' -f1 | sed 's/^m//') 
+                    B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
 
-    #             # main 명령 실행 결과를 result 변수에 저장
-    #             result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+                    # 파일에서 ./data 부분 제거
+                    relative_file="${file#./data}"
 
-    #             # result 값을 소수점 두 자리로 맞추어 저장
-    #             printf -v result "%.2f" "$result"
-    #             results+=("$blockSize,$A,$B,$i,$result")
-    #         fi
-    #     done
+                    # main 명령 실행 결과를 result 변수에 저장
+                    result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+
+                    # result 값을 소수점 두 자리로 맞추어 저장
+                    results+=("$blockSize,$A,$B,$i,$result")
+                fi
+            done
 
 
-    #     # csv 파일에 기록
-    #     echo "saving results to csv file"
-    #     printf "%s\n" "${results[@]}" >> "$csv_file"
+            # csv 파일에 기록
+            echo "saving results to csv file"
+            printf "%s\n" "${results[@]}" >> "$csv_file"
 
-    #     # mtx 파일들은 모두 삭제
-    #     rm -f ./data/$type/skinny/*.mtx
-    # done
+            # mtx 파일들은 모두 삭제
+            rm -f $type/skinny/*.mtx
+        done
+    fi
+    #####################################################
 
+    if [ "${mask[1]}" -eq "1" ]; then
+
+        ######## 실행 - long #######
+        echo "---- long ----"
+        csv_file="${type}${subdirs[1]}.csv"
+        echo "this $csv_file"
+
+        # 데이터셋 생성
+        echo "generating dataset for long"
+        python3 ./util/gen_long.py $command_default
+
+        # 실행시간 측정
+        for i in {1..3}; do
+            echo "set $i"
+            results=()
+            for file in $type/long/*.mtx; do
+                if [ -f "$file" ]; then
+                    echo "$file"
+                    base_name=$(basename "$file")
+                    A=$(echo "$base_name" | cut -d'_' -f1 | sed 's/^m//')
+                    B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
+
+                    # 파일에서 ./data 부분 제거
+                    relative_file="${file#./data}"
+
+                    # main 명령 실행 결과를 result 변수에 저장
+                    result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+
+                    # result 값을 소수점 두 자리로 맞추어 저장
+                    results+=("$blockSize,$A,$B,$i,$result")
+                fi
+            done
+
+
+            # csv 파일에 기록
+            echo "saving results to csv file"
+            printf "%s\n" "${results[@]}" >> "$csv_file"
+
+            # mtx 파일들은 모두 삭제
+            rm -f $type/long/*.mtx
+        done
+    fi
+    #######################################################
+
+    if [ "${mask[2]}" -eq "1" ]; then
+        ######## 실행 - stair #######
+        echo "---- stair ----"
+        csv_file="${type}${subdirs[2]}.csv"
+        echo "this $csv_file"
+
+        # 데이터셋 생성
+        echo "generating dataset for stair"
+        python3 ./util/gen_stair.py $command_default
+
+        # 실행시간 측정
+        for i in {1..3}; do
+            echo "set $i"
+            results=()
+            for file in $type/stair/*.mtx; do
+                if [ -f "$file" ]; then
+                    echo "$file"
+                    base_name=$(basename "$file")
+                    A=$(echo "$base_name" | cut -d'_' -f2)
+                    B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
+
+                    # 파일에서 ./data 부분 제거
+                    relative_file="${file#./data}"
+
+                    # main 명령 실행 결과를 result 변수에 저장
+                    result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+
+                    # result 값을 소수점 두 자리로 맞추어 저장
+                    results+=("$blockSize,$A,$B,$i,$result")
+                fi
+            done
+
+
+            # csv 파일에 기록
+            echo "saving results to csv file"
+            printf "%s\n" "${results[@]}" >> "$csv_file"
+
+            # mtx 파일들은 모두 삭제
+            rm -f $type/stair/*.mtx
+        done
+    fi
     ######################################################
 
-    # ######## 실행 - long #######
-    # echo "---- long ----"
-    # csv_file="${type}${subdirs[1]}.csv"
-    # echo "this $csv_file"
+    if [ "${mask[3]}" -eq "1" ]; then
+        ######## 실행 - zigzag #######
+        echo "---- zigzag ----"
+        csv_file="${type}${subdirs[3]}.csv"
+        echo "this $csv_file"
 
-    # # 데이터셋 생성
-    # echo "generating dataset for long"
-    # python3 ./util/gen_long.py $command_default
+        # 데이터셋 생성
+        echo "generating dataset for zigzag"
+        python3 ./util/gen_zigzag.py $command_default
 
-    # # 실행시간 측정
-    # for i in {1..3}; do
-    #     echo "set $i"
-    #     results=()
-    #     for file in $type/long/*.mtx; do
-    #         if [ -f "$file" ]; then
-    #             echo "$file"
-    #             base_name=$(basename "$file")
-    #             A=$(echo "$base_name" | cut -d'_' -f1 | sed 's/^m//')
-    #             B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
+        # 실행시간 측정
+        for i in {1..3}; do
+            echo "set $i"
+            results=()
+            for file in $type/zigzag/*.mtx; do
+                if [ -f "$file" ]; then
+                    echo "$file"
+                    base_name=$(basename "$file")
+                    A=$(echo "$base_name" | cut -d'_' -f2)
+                    B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
 
-    #             # 파일에서 ./data 부분 제거
-    #             relative_file="${file#./data}"
+                    # 파일에서 ./data 부분 제거
+                    relative_file="${file#./data}"
 
-    #             # main 명령 실행 결과를 result 변수에 저장
-    #             result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+                    # main 명령 실행 결과를 result 변수에 저장
+                    result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
 
-    #             # result 값을 소수점 두 자리로 맞추어 저장
-    #             printf -v result "%.2f" "$result"
-    #             results+=("$blockSize,$A,$B,$i,$result")
-    #         fi
-    #     done
+                    # result 값을 소수점 두 자리로 맞추어 저장
+                    results+=("$blockSize,$A,$B,$i,$result")
+                fi
+            done
 
 
-    #     # csv 파일에 기록
-    #     echo "saving results to csv file"
-    #     printf "%s\n" "${results[@]}" >> "$csv_file"
+            # csv 파일에 기록
+            echo "saving results to csv file"
+            printf "%s\n" "${results[@]}" >> "$csv_file"
 
-    #     # mtx 파일들은 모두 삭제
-    #     rm -f ./data/$type/long/*.mtx
-    # done
-
-    # ######################################################
-
-    ######## 실행 - stair #######
-    echo "---- stair ----"
-    csv_file="${type}${subdirs[2]}.csv"
-    echo "this $csv_file"
-
-    # 데이터셋 생성
-    echo "generating dataset for stair"
-    python3 ./util/gen_stair.py $command_default
-
-    # 실행시간 측정
-    for i in {1..3}; do
-        echo "set $i"
-        results=()
-        for file in $type/stair/*.mtx; do
-            if [ -f "$file" ]; then
-                echo "$file"
-                base_name=$(basename "$file")
-                A=$(echo "$base_name" | cut -d'_' -f2)
-                B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
-
-                # 파일에서 ./data 부분 제거
-                relative_file="${file#./data}"
-
-                # main 명령 실행 결과를 result 변수에 저장
-                result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
-
-                # result 값을 소수점 두 자리로 맞추어 저장
-                printf -v result "%.2f" "$result"
-                results+=("$blockSize,$A,$B,$i,$result")
-            fi
+            # mtx 파일들은 모두 삭제
+            rm -f $type/zigzag/*.mtx
         done
-
-
-        # csv 파일에 기록
-        echo "saving results to csv file"
-        printf "%s\n" "${results[@]}" >> "$csv_file"
-
-        # mtx 파일들은 모두 삭제
-        rm -f ./data/$type/stair/*.mtx
-    done
-
+    fi
     ######################################################
 
-    ######## 실행 - zigzag #######
-    echo "---- zigzag ----"
-    csv_file="${type}${subdirs[3]}.csv"
-    echo "this $csv_file"
+    if [ "${mask[4]}" -eq "1" ]; then
+        ######## 실행 - twoline_col #######
+        echo "---- twoline_col ----"
+        csv_file="${type}${subdirs[4]}.csv"
+        echo "this $csv_file"
 
-    # 데이터셋 생성
-    echo "generating dataset for zigzag"
-    python3 ./util/gen_zigzag.py $command_default
+        # 데이터셋 생성
+        echo "generating dataset for twoline_col"
+        python3 ./util/gen_twoline_col.py $command_default
 
-    # 실행시간 측정
-    for i in {1..3}; do
-        echo "set $i"
-        results=()
-        for file in $type/zigzag/*.mtx; do
-            if [ -f "$file" ]; then
-                echo "$file"
-                base_name=$(basename "$file")
-                A=$(echo "$base_name" | cut -d'_' -f2)
-                B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
+        # 실행시간 측정
+        for i in {1..3}; do
+            echo "set $i"
+            results=()
+            for file in $type/twoline_col/*.mtx; do
+                if [ -f "$file" ]; then
+                    echo "$file"
+                    base_name=$(basename "$file")
+                    A=$(echo "$base_name" | cut -d'_' -f2)
+                    B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
 
-                # 파일에서 ./data 부분 제거
-                relative_file="${file#./data}"
+                    # 파일에서 ./data 부분 제거
+                    relative_file="${file#./data}"
 
-                # main 명령 실행 결과를 result 변수에 저장
-                result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+                    # main 명령 실행 결과를 result 변수에 저장
+                    result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
 
-                # result 값을 소수점 두 자리로 맞추어 저장
-                printf -v result "%.2f" "$result"
-                results+=("$blockSize,$A,$B,$i,$result")
-            fi
+                    # result 값을 소수점 두 자리로 맞추어 저장
+                    results+=("$blockSize,$A,$B,$i,$result")
+                fi
+            done
+
+
+            # csv 파일에 기록
+            echo "saving results to csv file"
+            printf "%s\n" "${results[@]}" >> "$csv_file"
+
+            # mtx 파일들은 모두 삭제
+            rm -f $type/twoline_col/*.mtx
         done
-
-
-        # csv 파일에 기록
-        echo "saving results to csv file"
-        printf "%s\n" "${results[@]}" >> "$csv_file"
-
-        # mtx 파일들은 모두 삭제
-        rm -f ./data/$type/zigzag/*.mtx
-    done
-
+    fi
     ######################################################
 
+    if [ "${mask[5]}" -eq "1" ]; then
+        ######## 실행 - twoline_row #######
+        echo "---- twoline_row ----"
+        csv_file="${type}${subdirs[5]}.csv"
+        echo "this $csv_file"
 
-    ######## 실행 - twoline_col #######
-    echo "---- twoline_col ----"
-    csv_file="${type}${subdirs[4]}.csv"
-    echo "this $csv_file"
+        # 데이터셋 생성
+        echo "generating dataset for twoline_row"
+        python3 ./util/gen_twoline_row.py $command_default
 
-    # 데이터셋 생성
-    echo "generating dataset for twoline_col"
-    python3 ./util/gen_twoline_col.py $command_default
+        # 실행시간 측정
+        for i in {1..3}; do
+            echo "set $i"
+            results=()
+            for file in $type/twoline_row/*.mtx; do
+                if [ -f "$file" ]; then
+                    echo "$file"
+                    base_name=$(basename "$file")
+                    A=$(echo "$base_name" | cut -d'_' -f2)
+                    B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
 
-    # 실행시간 측정
-    for i in {1..3}; do
-        echo "set $i"
-        results=()
-        for file in $type/twoline_col/*.mtx; do
-            if [ -f "$file" ]; then
-                echo "$file"
-                base_name=$(basename "$file")
-                A=$(echo "$base_name" | cut -d'_' -f2)
-                B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
+                    # 파일에서 ./data 부분 제거
+                    relative_file="${file#./data}"
 
-                # 파일에서 ./data 부분 제거
-                relative_file="${file#./data}"
+                    # main 명령 실행 결과를 result 변수에 저장
+                    result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
 
-                # main 명령 실행 결과를 result 변수에 저장
-                result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
+                    # result 값을 소수점 두 자리로 맞추어 저장
+                    results+=("$blockSize,$A,$B,$i,$result")
+                fi
+            done
 
-                # result 값을 소수점 두 자리로 맞추어 저장
-                printf -v result "%.2f" "$result"
-                results+=("$blockSize,$A,$B,$i,$result")
-            fi
+
+            # csv 파일에 기록
+            echo "saving results to csv file"
+            printf "%s\n" "${results[@]}" >> "$csv_file"
+
+            # mtx 파일들은 모두 삭제
+            rm -f $type/twoline_row/*.mtx
         done
-
-
-        # csv 파일에 기록
-        echo "saving results to csv file"
-        printf "%s\n" "${results[@]}" >> "$csv_file"
-
-        # mtx 파일들은 모두 삭제
-        rm -f ./data/$type/twoline_col/*.mtx
-    done
-
-    ######################################################
-
-
-    ######## 실행 - twoline_row #######
-    echo "---- twoline_row ----"
-    csv_file="${type}${subdirs[5]}.csv"
-    echo "this $csv_file"
-
-    # 데이터셋 생성
-    echo "generating dataset for twoline_row"
-    python3 ./util/gen_twoline_row.py $command_default
-
-    # 실행시간 측정
-    for i in {1..3}; do
-        echo "set $i"
-        results=()
-        for file in $type/twoline_row/*.mtx; do
-            if [ -f "$file" ]; then
-                echo "$file"
-                base_name=$(basename "$file")
-                A=$(echo "$base_name" | cut -d'_' -f2)
-                B=$(echo "$base_name" | cut -d'_' -f3 | sed 's/\.mtx$//')  # 확장자 .mtx 제거
-
-                # 파일에서 ./data 부분 제거
-                relative_file="${file#./data}"
-
-                # main 명령 실행 결과를 result 변수에 저장
-                result=$(./main "$relative_file" "$blockSize" "$rhsCol" "1")
-
-                # result 값을 소수점 두 자리로 맞추어 저장
-                printf -v result "%.2f" "$result"
-                results+=("$blockSize,$A,$B,$i,$result")
-            fi
-        done
-
-
-        # csv 파일에 기록
-        echo "saving results to csv file"
-        printf "%s\n" "${results[@]}" >> "$csv_file"
-
-        # mtx 파일들은 모두 삭제
-        rm -f ./data/$type/twoline_row/*.mtx
-    done
+    fi
 done
